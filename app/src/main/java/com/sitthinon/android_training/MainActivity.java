805796f912b0f3528fdea0b6193560efd96d91be
N0ThingA11 <sitthinon.meesat@gmail.com>
2019-02-23 13:13:43 +0700

    After Launch

---
+++ app/src/main/java/com/sitthinon/android_training/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sitthinon.android_training;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
+
+
+    } //Main Method
+
+} //Main Class
