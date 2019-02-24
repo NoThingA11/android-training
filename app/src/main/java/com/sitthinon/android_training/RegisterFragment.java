372a3d402c4a612faed2afb87004d18b82aca600
N0ThingA11 <sitthinon.meesat@gmail.com>
2019-02-24 13:24:43 +0700

    After Launch

--- app/src/main/java/com/sitthinon/android_training/RegisterFragment.java
+++ app/src/main/java/com/sitthinon/android_training/RegisterFragment.java
@@ -1,13 +1,27 @@
 package com.sitthinon.android_training;
 
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.time.Instant;
 
 
 /**
@@ -15,6 +29,10 @@ import android.view.ViewGroup;
  */
 public class RegisterFragment extends Fragment {
 
+    //    Explicit
+    private boolean aBoolean = true;
+    private ImageView imageView;
+    private Uri uri;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -27,8 +45,47 @@ public class RegisterFragment extends Fragment {
 
 //        Create Toolbar
 
+        createToolbar();
+//        Choose Image
+        chooseImage();
+
+    }//Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == getActivity().RESULT_OK) {
+            aBoolean = false;
+            uri = data.getData();
+
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, false);
+                imageView.setImageBitmap(bitmap1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }//if check result code
+    }
+
+
+    private void chooseImage() {
+        imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Chose App"), 1);
+            }
+        });
+    }
+
+    private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
         ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
         ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -38,8 +95,61 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+        setHasOptionsMenu(true);
+    }
 
-    }//Main Method
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+            checkValue();
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void checkValue() {
+        MyAlert myAlert = new MyAlert(getActivity());
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+        String name = nameEditText.getText().toString().trim();
+        String user = userEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+
+        if (aBoolean) {
+//            Non Choose Image
+            myAlert.normalDialog("Non Choose Image", "Please Choose Avata");
+        } else if (name.equals("") || user.equals("") || password.equals("")) {
+//            Have Space
+            myAlert.normalDialog("Have Space ", "Pleasde Fill All Blank");
+        } else {
+//            Upload Image To Server
+            String pathImageString = null;
+            String[] strings = new String[]{MediaStore.Images.Media.DATA};
+            Cursor cursor = getActivity().getContentResolver().query(uri,strings,null,null,null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+                int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                pathImageString = cursor.getString(index);
+            } else {
+                pathImageString = uri.getPath();
+            }
+
+            Log.d("24FebV1","Path ==>" + pathImageString);
+            String nameImage = pathImageString.substring(pathImageString.lastIndexOf("/"));
+            Log.d("24FebV1","Name ==>" + nameImage);
+        }//if
+
+    }//check value
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu, menu);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
